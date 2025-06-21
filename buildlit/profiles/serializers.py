1e32425241d5076462314eb3a47baceae4717043
AnantSharma09 <robinjaiswal555@gmail.com>
2025-06-22 05:27:43 +0530

    Add complete serializer.py code

--- buildlit/profiles/serializers.py
+++ buildlit/profiles/serializers.py
@@ -8,3 +8,22 @@ class ProfileSerializer(serializers.ModelSerializer):
         read_only_fields = ['created_at']
 
 
+class SkillsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Skills
+        fields = '__all__'
+
+class SkillWeightageSerializer(serializers.ModelSerializer):
+    # for post/put requests, we need to use PrimaryKeyRelatedField
+    profile = serializers.PrimaryKeyRelatedField(queryset=Profile.objects.all())
+    skill =  serializers.PrimaryKeyRelatedField(queryset=Skills.objects.all())
+    # for get requests, we can use the SkillsSerializer to get full skill details
+    profile_data = ProfileSerializer(source='profile', read_only=True)
+    skill_data = SkillsSerializer(source='skill', read_only=True)
+
+
+  
+    class Meta:
+        model=SkillWeightage
+        fields= ['id','profile','weightage','created_at','profile_data','skill_data']
+        read_only_fields = ['created_at']
