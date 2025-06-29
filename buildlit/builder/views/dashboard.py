06162686082c0d5c7deb89d8189ea510341ed764
AnantSharma09 <robinjaiswal555@gmail.com>
2025-06-29 20:46:05 +0530

    Added startup profile and dashboard in builder app which will show builder dashboard and functionalities

---
+++ buildlit/builder/views/dashboard.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+# @api_view(['GET'])
+# @permission_classes([IsAuthenticated])
+# def builder_dashboard(request):
+    # 1. Verify role == builder
+    # 2. Get StartupProfile
+    # 3. Get StartupTeam
+    # 4. Get builder’s posts (from posts app)
+    # 5. Get top joiners (from joiner/leaderboard app)
+    # 6. Get competitions created (from competitions app)
+
+    # 7. Return everything as JSON
