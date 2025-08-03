2c87f0aad5b8b629721bfbded9dcfb2b01855af5
AnantSharma09 <robinjaiswal555@gmail.com>
2025-08-03 14:49:42 +0530

    updated authapp

--- buildlit/authapp/views.py
+++ buildlit/authapp/views.py
@@ -1,26 +1,26 @@
-from rest_framework import generics
-from django.contrib.auth.models import User
+from rest_framework import generics, status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import RegisterSerializer
+from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
+from profiles.models import Profile
+from .serializers import RegisterSerializer
 
-# Signup
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = [AllowAny]
     serializer_class = RegisterSerializer
 
-# Optional: Logout view
-class LogoutView(generics.GenericAPIView):
-    def post(self, request):
-        try:
-            refresh_token = request.data["refresh"]
-            token = RefreshToken(refresh_token)
-            token.blacklist()
-            return Response(status=status.HTTP_205_RESET_CONTENT)
-        except Exception as e:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()  # creates user
 
+        
+        Profile.objects.create(user=user)
+
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }, status=status.HTTP_201_CREATED)
